91f1d1fed7c96a95e1537a18a15fa138bf2f8a7a
Juan David Amaya <jdaa33_@hotmail.com>
2023-05-16 22:19:42 -0500

    Cors

--- routes/api.php
+++ routes/api.php
@@ -15,28 +15,29 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::middleware(['cors'])->group(function (){
+    Route::post('auth/register', [AuthController::class, 'register']);
+    Route::post('auth/login', [AuthController::class, 'login'])->name('login');
 
-Route::post('auth/register', [AuthController::class, 'register']);
-Route::post('auth/login', [AuthController::class, 'login'])->name('login');
+    Route::middleware(['auth:api'])->group(function (){
 
-Route::middleware(['auth:api'])->group(function (){
+        Route::prefix('auth')->group(function(){
+            Route::get('logout', [AuthController::class, 'logout']);
+            Route::get('me', [AuthController::class, 'me']);
 
-    Route::prefix('auth')->group(function(){
-        Route::get('logout', [AuthController::class, 'logout']);
-        Route::get('me', [AuthController::class, 'me']);
+        });
 
-    });
+        Route::get('companies', [CompanyController::class, 'index']);
 
-    Route::get('companies', [CompanyController::class, 'index']);
+        /**
+         * Admin routes
+         */
 
-    /**
-     * Admin routes
-     */
-
-    Route::prefix('admin')->middleware('admin')->group(function (){
-        Route::apiResource('users', UserController::class)->only(['index', 'destroy']);
-        Route::apiResource('companies', CompanyController::class)->only(['show', 'store', 'destroy']);
-        Route::apiResource('products', ProductController::class)->only(['destroy', 'store']);
+        Route::prefix('admin')->middleware('admin')->group(function (){
+            Route::apiResource('users', UserController::class)->only(['index', 'destroy']);
+            Route::apiResource('companies', CompanyController::class)->only(['show', 'store', 'destroy']);
+            Route::apiResource('products', ProductController::class)->only(['destroy', 'store']);
+        });
     });
 });
 
